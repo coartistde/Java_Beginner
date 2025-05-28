8be4a67492ba3f15b51ca5e88a942ad5e8f0403c
coartistde <coarttide073@gmail.com>
2025-05-28 23:54:43 +0900

    소스 코드 수정

--- CvsJava.java
+++ CvsJava.java
@@ -9,7 +9,7 @@
 public class CvsJava {
     public static void main(String args[]) {
         int i = 1, sum = 0;
-        for(i = 1; i <=10; i++) {
+        for(i = 1; i <= 10; i++) {
             sum += i;
         }
     }
