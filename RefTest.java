89359dfc611f949d19670a8e66d617fdfd7c891e
coartistde <coarttide073@gmail.com>
2025-06-04 00:23:34 +0900

    [소스2-8] 참조형의 예

---
+++ RefTest.java
@@ -0,0 +1,15 @@
+/*
+ * (교재 42쪽)
+ * 제2장 Java 기본 문법
+ * 
+ * [소스 2-8] 참조형의 예
+ * 기본형을 제외한 모든 자료형 = 참조형
+ * ex) 클래스형 변수, 배열형의 변수, 문자열형의 변수
+ */
+
+public class RefTest {
+    public static void main(String args[]) {
+        int anArray[];      // 정수형 배열형 변수
+        String szString;    // 문자열형 변수(String 클래스형)
+    }
+}
