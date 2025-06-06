ffbf80db0715ac7750cf4a5cd40966b8b42d2bf7
coartistde <coarttide073@gmail.com>
2025-06-06 23:27:17 +0900

    [소스 2-12] final을 이용한 상수 선언의 예

---
+++ FinalTest.java
@@ -0,0 +1,16 @@
+/*
+ * (교재 44쪽)
+ * 제2정 Java 기본 문법
+ * 
+ * [소스 2-12] final을 이용한 상수 선언의 예
+ * 변수(variable) = '변할 수 있는 값'을 저장.
+ * 상수(constant) = '변하지 않는 값'을 저장.
+ * 변수나 상수는 반드시 자료형을 가짐.
+ * 상수 = 리터럴 자체로 사용 or final 키워드로 정의
+ */
+
+public class FinalTest {
+    public static void main(String args[]) {
+        final int nConst = 3;   // 정수형 상수 선언 및 초기화
+    }
+}
